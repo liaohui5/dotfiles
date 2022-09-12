02ac9866a142e63f76d795e0260d8a3e550fcdcd
liaohui5 <liaohui5@foxmail.com>
2022-09-13 07:48:39 +0800

    增加了自动读取自动保存的插件

--- nvim/lua/plugin-configs/auto-save.lua
+++ nvim/lua/plugin-configs/auto-save.lua
@@ -3,6 +3,7 @@
 -- │  docs: https://github.com/Pocco81/auto-save.nvim                             │
 -- ╰──────────────────────────────────────────────────────────────────────────────╯
 local autosave = loadModule("auto-save", "plugins-config");
+local utils    = loadModule("auto-save.utils.data", "plugins-config");
 
 autosave.setup({
   enabled           = true,
@@ -17,15 +18,19 @@ autosave.setup({
     end,
   },
   condition         = function(buf)
-    local disabled_buffers = {
+    local disabled_buffers  = {
       "NvimTree",
       "TelescopePrompt",
       "aerial",
       "spectre_panel"
     };
-    local fn    = vim.fn
-    local utils = require("auto-save.utils.data")
-    if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), disabled_buffers) then
+    local disabled_buftypes = {
+      "prompt"
+    }
+    local modifiable        = vim.fn.getbufvar(buf, "&modifiable")
+    local filetype          = vim.fn.getbufvar(buf, "&filetype")
+    local buftype           = vim.fn.getbufvar(buf, "&buftype")
+    if modifiable == 1 and utils.not_in(filetype, disabled_buffers) and utils.not_in(buftype, disabled_buftypes) then
       return true
     end
     return false
