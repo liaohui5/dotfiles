c1f061e24d9b79595ac5ad4c9d0a3b1ea3794c8d
liaohui5 <liaohui5@foxmail.com>
2022-09-18 00:15:00 +0800

    serfingkeys: 修改了serfingkeys快捷键

--- serfingkeys/config.js
+++ serfingkeys/config.js
@@ -1,19 +1,50 @@
 //////////////////////////////////////////////////////////////////////////////////////
 // ╭──────────────────────────────────────────────────────────────────────────────╮ //
-// │                     _              _     _           _ _                     │ //
+// │                                _                                             │ //
+// │                             __| | ___   ___ ___                              │ //
+// │                            / _` |/ _ \ / __/ __|                             │ //
+// │                           | (_| | (_) | (__\__ \                             │ //
+// │                           \__,_|\___/ \___|___/                              │ //
+// ╰──────────────────────────────────────────────────────────────────────────────╯ //
+//////////////////////////////////////////////////////////////////////////////////////
+// github:       https://github.com/brookhong/Surfingkeys
+// README:       https://github.com/brookhong/Surfingkeys/blob/master/README_CN.md
+// API:          https://github.com/brookhong/Surfingkeys/blob/master/docs/API.md
+// conf-example: https://github.com/b0o/surfingkeys-conf
+
+//////////////////////////////////////////////////////////////////////////////////////
+// ╭──────────────────────────────────────────────────────────────────────────────╮ //
+// │                    __                 _   _                                  │ //
+// │                  / _|_   _ _ __   ___| |_(_) ___  _ __  ___                  │ //
+// │                 | |_| | | | '_ \ / __| __| |/ _ \| '_ \/ __|                 │ //
+// │                 |  _| |_| | | | | (__| |_| | (_) | | | \__ \                 │ //
+// │                 |_|  \__,_|_| |_|\___|\__|_|\___/|_| |_|___/                 │ //
+// ╰──────────────────────────────────────────────────────────────────────────────╯ //
+//////////////////////////////////////////////////////////////////////////////////////
+function each(array, callback) {
+  var item;
+  for (var i = 0, l = array.length; i < l; i++) {
+    item = array[i];
+    typeof callback === "function" && callback(item, i);
+  }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+// ╭──────────────────────────────────────────────────────────────────────────────╮ //
+// │             _              _     _           _ _                             │ //
 // │            | | _____ _   _| |__ (_)_ __   __| (_)_ __   __ _ ___             │ //
 // │            | |/ / _ \ | | | '_ \| | '_ \ / _` | | '_ \ / _` / __|            │ //
 // │            |   <  __/ |_| | |_) | | | | | (_| | | | | | (_| \__ \            │ //
 // │            |_|\_\___|\__, |_.__/|_|_| |_|\__,_|_|_| |_|\__, |___/            │ //
-// │                   |___/                             |___/                    │ //
+// │                    |___/                             |___/                   │ //
 // ╰──────────────────────────────────────────────────────────────────────────────╯ //
 //////////////////////////////////////////////////////////////////////////////////////
 // 将<c-p><c-n>映射到<c-j><c-k>
-api.cmap('<Ctrl-j>', '<Ctrl-n>');
-api.cmap('<Ctrl-k>', '<Ctrl-p>');
+api.cmap("<Ctrl-j>", "<Ctrl-n>");
+api.cmap("<Ctrl-k>", "<Ctrl-p>");
 
 // ctrl+p: 直接 google 搜索
-api.map('<Ctrl-p>', 'og');
+api.map("<Ctrl-p>", "og");
 
 // 左右切换tab标签页: H, L
 api.map("H", "E");
@@ -22,50 +53,79 @@ api.map("L", "R");
 // ctrl+u/ctrl+d 上下翻页
 api.map("<Ctrl-u>", "u");
 api.map("<Ctrl-d>", "d");
-// api.mapkey('<Ctrl-d>', '#9Forward half page', function() { Normal.feedkeys('5j'); });
-// api.mapkey('<Ctrl-u>', '#9Back half page', function() { Normal.feedkeys('5k'); });
+// api.mapkey('<Ctrl-d>', '#9Forward half page', function() { api.Normal.feedkeys('5j'); });
+// api.mapkey('<Ctrl-u>', '#9Back half page', function() { api.Normal.feedkeys('5k'); });
 
 // 向左/右移动tab
-api.map('>', '>>');
-api.map('<', '<<');
+api.map(">", ">>");
+api.map("<", "<<");
 
 // 打开粘贴班中的 url
-api.mapkey('p', "Open the clipboard's URL in the current tab", function() {
-  api.Clipboard.read(function(response) {
+api.mapkey("p", "Open the clipboard's URL in the current tab", function () {
+  api.Clipboard.read(function (response) {
     window.location.href = response.data;
   });
 });
 
-// 添加bilibili搜索(s: 选中直接搜索 o: 输入搜索)
-var biliSearchURL = 'https://search.bilibili.com/all?keyword=';
-api.addSearchAlias('B', 'bilibili', biliSearchURL, 's');
-api.addSearchAlias('B', 'bilibili', biliSearchURL, 'o', biliSearchURL);
-
 // 选择 tabs
-api.mapkey('<Space>', 'Choose a tab with omnibar', function() {
-  api.Front.openOmnibar({type: "Tabs"});
+api.mapkey("<Space>", "Choose a tab with omnibar", function () {
+  api.Front.openOmnibar({ type: "Tabs" });
 });
 
-// 直接打开url
-var urls = [
-    {
-        key: 'ol',
-        desc: 'Open blog',
-        url: 'https://liaohui5.github.io/study-notes/#/',
-    },
-    {
-        key: 'oG',
-        desc: 'Open github profile',
-        url: 'https://github.com/liaohui5',
-    }
+// 自定义搜索
+var searchUrls = [
+  {
+    key: "B",
+    engine: "Bilibili",
+    url: "https://search.bilibili.com/all?keyword=",
+    favicon_url: "https://www.bilibili.com/favicon.ico",
+  },
+  {
+    key: "N",
+    engine: "npm",
+    url: "https://www.npmjs.com/search?q=",
+    favicon_url:
+      "https://static.npmjs.com/7a7ffabbd910fc60161bc04f2cee4160.png",
+  },
+  {
+    key: "M",
+    engine: "MDN",
+    url: "https://developer.mozilla.org/en-US/search?q=",
+    favicon_url: "https://developer.mozilla.org/apple-touch-icon.6803c6f0.png",
+  },
 ];
-var item;
-for (var i = 0, l = urls.length; i < l; i++) {
-    item = urls[i];
-    api.mapkey(item.key, item.desc, function() {
-       api.tabOpenLink(item.url)
-    })
-}
+each(searchUrls, function (item) {
+  api.addSearchAlias(
+    item.key,
+    item.engine,
+    item.url,
+    "o",
+    item.url,
+    Function.prototype,
+    "",
+    {
+      favicon_url: item.favicon_url,
+    }
+  );
+});
+
+// o[number] 直接打开url
+var urls = [
+  {
+    desc: "Open blog",
+    url: "https://liaohui5.github.io/study-notes/#/",
+  },
+  {
+    desc: "Open github profile",
+    url: "https://github.com/liaohui5",
+  },
+];
+each(urls, function (item, i) {
+  var key = "o" + (i + 1);
+  api.mapkey(key, item.desc, function () {
+    api.tabOpenLink(item.url);
+  });
+});
 
 //////////////////////////////////////////////////////////////////////////////////////
 // ╭──────────────────────────────────────────────────────────────────────────────╮ //
