252f936a42d653830f9a7f2be75875db4a35ccd6
liaohui5 <liaohui5@foxmail.com>
2022-12-29 22:55:59 +0800

    nvim:修改了options.lua

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -10,54 +10,55 @@
 --  | '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |
 --   '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'
 -- 在这里定义启动 neovim 的选项
-vim.opt.shortmess:append("c"); -- 禁用 neovim 启动页介绍
-vim.opt.compatible      = false; -- 缺省vi兼容模式,不能用退格.设置为不兼容模式
-vim.opt.title           = true; -- 显示 title
-vim.opt.cursorline      = true; -- 高亮当前行
-vim.opt.list            = false; -- 是否显示空白符号
-vim.opt.clipboard       = "unnamedplus"; -- 使用系统粘贴板
-vim.opt.updatetime      = 100; -- 更新间隔时间(ms), 比如按下 <Leader> 键后没有再
-vim.opt.timeoutlen      = 0; -- 按其他键多久后自动取消 <Leader>
-vim.opt.signcolumn      = "yes"; -- 显示图标栏
-vim.opt.undofile        = true; -- 开启撤销永久化
-vim.opt.laststatus      = 3; -- 命令行状态 && 命令行高度
-vim.opt.cmdheight       = 1; -- 底部命令行的高度为1
-vim.opt.ruler           = false; -- 底部的行号等显示
-vim.opt.number          = true; -- 显示行号
-vim.opt.relativenumber  = false; -- 相对当前行的行号
-vim.opt.numberwidth     = 4; -- 行号显示的宽度
-vim.opt.expandtab       = true; -- 将 tab 转空格
-vim.opt.tabstop         = 2; -- 一个tab设置为2空格的宽度
-vim.opt.softtabstop     = 2; -- 一个tab设置为2空格的宽度
-vim.opt.shiftwidth      = 2; -- 一个tab设置为2空格的宽度
-vim.opt.softtabstop     = 2; -- 一个tab设置为2空格的宽度
-vim.opt.wrap            = false; -- 不自动换行显示
-vim.opt.autoindent      = true; -- 自动缩进
-vim.opt.smarttab        = true; -- 会测试智能缩进
-vim.opt.encoding        = "utf-8"; -- 设置文件编码
-vim.opt.fileencodings   = "utf-8"; -- 设置文件编码
-vim.opt.fileencoding    = "utf-8"; -- 设置文件编码
-vim.opt.termencoding    = "utf-8"; -- 设置文件编码
-vim.opt.autoread        = true; -- 自动读取
-vim.opt.autowriteall    = true; -- 自动保存
-vim.opt.splitbelow      = true; -- 允许上下拆分窗口
-vim.opt.splitright      = true; -- 允许左右拆分窗口
-vim.opt.termguicolors   = true; -- terminal 颜色
-vim.opt.undofile        = true; -- 允许撤回文件
-vim.opt.hlsearch        = true; -- 搜索时高亮匹配到的字符
-vim.opt.wrapscan        = true; -- 扫描到文件两端时重新搜索
-vim.opt.smartcase       = true; -- 搜索时候区分大小写
-vim.opt.swapfile        = false; -- 不创建 swap 文件
-vim.opt.writebackup     = false; -- 不创建备份文件
-vim.opt.showmatch       = true; -- 当输入一个左括号时自动匹配右括号
-vim.opt.autochdir       = false; -- 自动切换当前目录为当前文件所在的目录
-vim.opt.hidden          = true; -- hidden
-vim.opt.fillchars       = "eob: "; -- 影藏空白区域~符号
-vim.opt.foldlevel       = 99; -- 打开文件时,不要自动折叠
-vim.opt.foldlevelstart  = 99; -- 打开文件时,不要自动折叠
-vim.opt.foldenable      = true; -- 开启折叠功能
-vim.opt.foldmethod      = "indent"; -- 根据缩进折叠 indent | expr | manual
--- vim.opt.foldexpr        = "nvim_treesitter#foldexpr()"; -- 使用 treesitter 来折叠
+local opt          = vim.opt;
+-- opt.shortmess      = "I"; -- 禁用 neovim 启动页介绍
+opt.compatible     = false; -- 缺省vi兼容模式,不能用退格.设置为不兼容模式
+opt.title          = true; -- 显示 title
+opt.cursorline     = true; -- 高亮当前行
+opt.list           = false; -- 是否显示空白符号
+opt.clipboard      = "unnamedplus"; -- 使用系统粘贴板
+opt.updatetime     = 100; -- 更新间隔时间(ms), 比如按下 <Leader> 键后没有再
+opt.timeoutlen     = 0; -- 按其他键多久后自动取消 <Leader>
+opt.signcolumn     = "yes"; -- 显示图标栏
+opt.undofile       = true; -- 开启撤销永久化
+opt.laststatus     = 3; -- 命令行状态 && 命令行高度
+opt.cmdheight      = 1; -- 底部命令行的高度为1
+opt.ruler          = false; -- 底部的行号等显示
+opt.number         = true; -- 显示行号
+opt.relativenumber = false; -- 相对当前行的行号
+opt.numberwidth    = 4; -- 行号显示的宽度
+opt.expandtab      = true; -- 将 tab 转空格
+opt.tabstop        = 2; -- 一个tab设置为2空格的宽度
+opt.softtabstop    = 2; -- 一个tab设置为2空格的宽度
+opt.shiftwidth     = 2; -- 一个tab设置为2空格的宽度
+opt.softtabstop    = 2; -- 一个tab设置为2空格的宽度
+opt.wrap           = false; -- 不自动换行显示
+opt.autoindent     = true; -- 自动缩进
+opt.smarttab       = true; -- 会测试智能缩进
+opt.encoding       = "utf-8"; -- 设置文件编码
+opt.fileencodings  = "utf-8"; -- 设置文件编码
+opt.fileencoding   = "utf-8"; -- 设置文件编码
+opt.termencoding   = "utf-8"; -- 设置文件编码
+opt.autoread       = true; -- 自动读取
+opt.autowriteall   = true; -- 自动保存
+opt.splitbelow     = true; -- 允许上下拆分窗口
+opt.splitright     = true; -- 允许左右拆分窗口
+opt.termguicolors  = true; -- terminal 颜色
+opt.undofile       = true; -- 允许撤回文件
+opt.hlsearch       = true; -- 搜索时高亮匹配到的字符
+opt.wrapscan       = true; -- 扫描到文件两端时重新搜索
+opt.smartcase      = true; -- 搜索时候区分大小写
+opt.swapfile       = false; -- 不创建 swap 文件
+opt.writebackup    = false; -- 不创建备份文件
+opt.showmatch      = true; -- 当输入一个左括号时自动匹配右括号
+opt.autochdir      = false; -- 自动切换当前目录为当前文件所在的目录
+opt.hidden         = true; -- hidden
+opt.fillchars      = "eob: "; -- 影藏空白区域~符号
+opt.foldlevel      = 99; -- 打开文件时,不要自动折叠
+opt.foldlevelstart = 99; -- 打开文件时,不要自动折叠
+opt.foldenable     = true; -- 开启折叠功能
+opt.foldmethod     = "indent"; -- 根据缩进折叠 indent | expr | manual
+-- opt.foldexpr        = "nvim_treesitter#foldexpr()"; -- 使用 treesitter 来折叠
 
 -- 全局变量
 vim.g.loaded_matchparen = false; -- 禁止自动匹配高亮括号
@@ -97,14 +98,13 @@ for _, plugin in pairs(default_plugins) do
 end
 
 -- 禁止的 provider, 注释表示开启
-local default_providers = {
+local disable_default_providers = {
   -- "python3",
   -- "node",
   "ruby",
   "perl"
 }
 
-for _, provider in ipairs(default_providers) do
+for _, provider in ipairs(disable_default_providers) do
   vim.g["loaded_" .. provider .. "_provider"] = 0;
 end
-
