b463b5bdccd50fec4906c4fb5bb80368ae59434a
liaohui5 <liaohui5@foxmail.com>
2023-08-13 21:33:55 +0800

    禁用默认的 keymaps 和 autocmds

--- nvim/lua/plugins/lazyvim.lua
+++ nvim/lua/plugins/lazyvim.lua
@@ -1,8 +1,15 @@
+---------------------------------------------------------------------------------
+-- 配置 LazyVim 内置的一些内容
+-- LazyVim default contents, if need, can copy to keymaps and autocmds
+-- https://github1s.com/LazyVim/LazyVim/blob/HEAD/lua/lazyvim/config/autocmds.lua
+-- https://github1s.com/LazyVim/LazyVim/blob/HEAD/lua/lazyvim/config/keymaps.lua#L43
+---------------------------------------------------------------------------------
 return {
-    "LazyVim/LazyVim",
-    opts = {
-        defaults = {
-            keymaps = false, -- disabled LazyVim default keymaps
-        },
+  "LazyVim/LazyVim",
+  opts = {
+    defaults = {
+      keymaps = false,        -- disabled LazyVim default keymaps
+      autocmds = false,       -- disabled LazyVim default autocmds
     },
+  },
 }
