d0ad0d353995b1d9b873d0cf932de33dbcfe0f70
liaohui5 <liaohui5@foxmail.com>
2023-08-14 23:53:46 +0800

    nvim:更新了lspconfig servers

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -15,4 +15,31 @@ return {
             mode = { "n", "v" },
         }
     end,
+    opts = {
+        servers = {
+            volar = { -- better vue syntax supported
+                filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
+            },
+            emmet_ls = {
+                filetypes = {
+                    "html",
+                    "typescriptreact",
+                    "javascriptreact",
+                    "css",
+                    "sass",
+                    "scss",
+                    "less",
+                    "markdown",
+                },
+                init_options = { -- better html/css snippets supported
+                    html = {
+                        options = {
+                            -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L26
+                            ["bem.enabled"] = true,
+                        },
+                    },
+                },
+            },
+        },
+    },
 }
