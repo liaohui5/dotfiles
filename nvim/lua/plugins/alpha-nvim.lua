12b92831c6bdff746496732c8c1cb1af44724145
liaohui5 <liaohui5@foxmail.com>
2023-08-24 23:24:26 +0800

    nvim:修改了nvim-alpha插件配置

--- nvim/lua/plugins/alpha-nvim.lua
+++ nvim/lua/plugins/alpha-nvim.lua
@@ -9,15 +9,16 @@ return {
         local dashboard = require("alpha.themes.dashboard")
         local section = dashboard.section
         local header = {
-            " ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗",
-            " ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║",
-            " ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║",
-            " ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║",
-            " ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║",
-            " ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝",
+            "███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗",
+            "████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║",
+            "██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║",
+            "██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║",
+            "██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║",
+            "╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝",
         }
         section.header.val = header
         section.footer.val = ""
+        -- stylua: ignore
         section.buttons.val = {
             dashboard.button("l", "󰥔  " .. "Load last session", "<cmd>SessionManager load_last_session<cr>"),
             dashboard.button("p", "  " .. "Pick session", "<cmd>SessionManager load_session<cr>"),
@@ -25,6 +26,7 @@ return {
             dashboard.button("r", "  " .. "Recent files", "<cmd>Telescope oldfiles<cr>"),
             dashboard.button("c", "  " .. "Configurations", "<cmd>edit $MYVIMRC<cr>"),
             dashboard.button("P", "  " .. "Plugins", "<cmd>Lazy<cr>"),
+            dashboard.button("H", "  " .. "Github", "<cmd>lua require('link-visitor').visit('https://github.com/liaohui5/dotfiles')<cr>"),
             dashboard.button("q", "  " .. "Quit", "<cmd>quitall<cr>"),
         }
         for _, button in ipairs(section.buttons.val) do
@@ -35,7 +37,7 @@ return {
         section.buttons.opts.hl = "AlphaButtons"
         section.footer.opts.hl = "AlphaFooter"
         dashboard.opts.layout = {
-            { type = "padding", val = 8 },
+            { type = "padding", val = 6 },
             section.header,
             { type = "padding", val = 6 },
             section.buttons,
