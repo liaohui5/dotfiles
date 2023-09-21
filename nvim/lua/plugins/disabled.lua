037a1e21cec08e6d4fe120482f295020379da9fa
liaohui5 <liaohui5@foxmail.com>
2023-09-21 13:02:14 +0800

    nvim:增加了自动匹配光标下的字符高亮显示插件

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -19,12 +19,11 @@ return disable_plugins({
     "echasnovski/mini.comment",            -- 注释插件(用习惯了其他的,不喜欢这个)
     "echasnovski/mini.indentscope",        -- 缩进显示
     "lukas-reineke/indent-blankline.nvim", -- 缩进显示
-    "RRethy/vim-illuminate",               -- 自动高亮匹配
     "SmiteshP/nvim-navic",                 -- 顶部代码导航
     "folke/persistence.nvim",              -- session 管理插件(功能太少, 不如 neovim-session-manager)
     "folke/noice.nvim",                    -- UI 美化
     "rcarriga/nvim-notify",                -- 优化通知消息 UI美化
     "zbirenbaum/copilot.lua",              -- copilot AI 生成代码
     "zbirenbaum/copilot-cmp",              -- copilot AI 生成代码
-    "echasnovski/mini.surround",           -- 快速操作两边字符
+    "echasnovski/mini.surround",           -- 快速操作两边字符(操作不如 nvim-surround 方便)
 })
---
+++ nvim/lua/plugins/vim-illuminate.lua
@@ -0,0 +1,25 @@
+------------------------------------------------------------
+-- 自动高亮匹配光标下的单词
+-- github: https://github.com/RRethy/vim-illuminate
+------------------------------------------------------------
+
+return {
+    "RRethy/vim-illuminate",
+    opts = function(_, opts)
+        -- https://neovim.io/doc/user/api.html#nvim_set_hl()
+        local hl = vim.api.nvim_set_hl
+        local highlights = {
+            bg = "NONE",
+            bold = true,
+            underline = true,
+        }
+        hl(0, "IlluminatedWordText", highlights)
+        hl(0, "IlluminatedWordRead", highlights)
+        hl(0, "IlluminatedWordWrite", highlights)
+        return vim.tbl_deep_extend("force", opts, {
+            providers = {
+                "treesitter",
+            },
+        })
+    end,
+}
