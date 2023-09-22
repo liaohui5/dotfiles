5593ff41eb727b6b594c516fa43c7a4c98061be5
liaohui5 <liaohui5@foxmail.com>
2023-09-22 19:10:07 +0800

    nvim:更新了 lspconfig, 添加了 bashls 支持

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -59,6 +59,13 @@ return {
                     },
                 },
             },
+
+            bashls = {
+                filetypes = {
+                    "sh",
+                    "bash",
+                },
+            },
         },
     },
 }
