b6b4df4c96fd08bc4ca4b58a95bdfe7d3d9e68a2
liaohui5 <liaohui5@foxmail.com>
2023-09-23 17:09:34 +0800

    nvim:修复了注释错误

--- nvim/lua/plugins/im-select.lua
+++ nvim/lua/plugins/im-select.lua
@@ -1,20 +1,16 @@
 --------------------------------------------------------------
--- 多文件搜索/替换,有 UI 界面
+-- 自动切换输入法
 -- github : https://github.com/keaising/im-select.nvim
 --------------------------------------------------------------
 
 return {
-    -- 自动切换输入法
     "keaising/im-select.nvim",
     event = "VeryLazy",
-
-    -- stylua: ignore
     opts = {
         -- MacOS 默认的英文输入法
         default_im_select = "com.apple.keylayout.ABC",
 
-        -- 进入插入模式时, 是否自动恢复上一次输入法状态 0:恢复 1:禁用
-        set_previous_events = {}
-    }
-,
+        -- 自动恢复上一次输入法状态事件(如:InsertEnter)
+        set_previous_events = {},
+    },
 }
