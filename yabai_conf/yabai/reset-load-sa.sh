b2f05bb6494fae85c84bfee2edc8444ce48611f4
liaohui5 <liaohui5@foxmail.com>
2023-10-29 18:13:37 +0800

    zsh&yabai:弃用oh-my-zsh,改用starship

--- yabai_conf/yabai/reset-load-sa.sh
+++ yabai_conf/yabai/reset-load-sa.sh
@@ -1,5 +1,5 @@
 #!/usr/local/bin/bash
 
 # 由于每次更新都需要手动更新 这个 sudoers/yabai 所有写了这个脚本,
-# 注意: 需要 root 权限运行
+# 注意: 需要 root 权限运行, 注意这个 "liaohui5" 是你的用户名, 不能用 $(whoami), 因为需要root权限,会变成root
 echo "liaohui5 ALL=(root) NOPASSWD: sha256: $(shasum -a 256 $(which yabai))" >/private/etc/sudoers.d/yabai
