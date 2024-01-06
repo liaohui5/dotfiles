a893ca3329c9bffcf4ced153e445c2938d3e798b
liaohui5 <liaohui5@foxmail.com>
2024-01-06 18:43:33 +0800

    nvim:更新了插件

--- .shell-scripts/alias.sh
+++ .shell-scripts/alias.sh
@@ -60,8 +60,7 @@ alias bm='bun run mock'
 
 # neovim: https://github.com/neovim/neovim
 # helix:  https://helix-editor.com/
-alias vi='vim'
-alias vim='nvim'
+alias v='nvim'
 
 # enable or disable clashx proxies
 alias setproxy='export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890'
