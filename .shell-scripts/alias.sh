71d52dd5908954f6a0bbf9c790fa1b7f1e7caa2f
liaohui5 <liaohui5@foxmail.com>
2024-03-28 02:49:21 +0800

    mpv:修改了mpv配置文件

--- .shell-scripts/alias.sh
+++ .shell-scripts/alias.sh
@@ -68,5 +68,5 @@ alias copypath='echo $(pwd) | pbcopy'
 # enable or disable clashx proxies
 alias setproxy='export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890'
 alias resetproxy="export https_proxy='' http_proxy='' all_proxy=''"
-setproxy
+resetproxy
 
