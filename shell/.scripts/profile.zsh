2f55858283cd82b17e19b358553af2d79479f48f
secret <secretx500@outlook.com>
2025-04-03 18:58:13 +0800

    setup(zsh): add configurations for zoxide, fzf, adb, pyenv, pnpm, and fnm
    - Add zoxide configuration for directory navigation
    - Set up fzf for fuzzy finding
    - Configure adb for Android development
    - Add pyenv setup for Python version management
    - Set up pnpm for package management
    - Configure fnm for Node.js version management

--- shell/.scripts/profile.zsh
+++ shell/.scripts/profile.zsh
@@ -47,3 +47,58 @@ if has-command 'yazi'; then
   }
   alias fm='yazi_fm'
 fi
+
+###############################################################
+# zoxide
+# https://github.com/ajeetdsouza/zoxide
+###############################################################
+if has-command 'zoxide'; then
+  eval "$(zoxide init zsh)"
+fi
+
+###############################################################
+# fzf
+# https://github.com/junegunn/fzf
+###############################################################
+if has-command 'fzf'; then
+  source <(fzf --zsh)
+fi
+
+################################################################
+# adb
+# https://developer.android.com/studio/command-line/adb.html
+################################################################
+local android_home_path="$HOME/Library/Android/sdk"
+if [[ -d "$android_home_path" ]]; then
+  export ANDROID_HOME="$android_home_path"
+  export PATH=$PATH:"$android_home_path/tools"
+  export PATH=$PATH:"$android_home_path/platform-tools"
+fi
+
+################################################################
+# pyenv
+# https://github.com/pyenv/pyenv
+################################################################
+if has-command 'pyenv'; then
+  export PYENV_ROOT="$HOME/.pyenv"
+  export PATH="$PYENV_ROOT/bin:$PATH"
+  export PATH="$PYENV_ROOT/shims:$PATH"
+  eval "$(pyenv init -)"
+fi
+
+################################################################
+# pnpm
+# https://pnpm.io/
+################################################################
+if has-command 'pnpm'; then
+  export PNPM_HOME="$HOME/.pnpm_store"
+  export PATH="$PNPM_HOME:$PATH"
+fi
+
+################################################################
+# fnm
+# https://github.com/Schniz/fnm
+################################################################
+if has-command 'fnm'; then
+  eval "$(fnm env --use-on-cd --version-file-strategy=recursive --shell zsh)"
+fi
